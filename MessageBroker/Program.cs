7a958bea27a1818a0114faeb93ec1970a81d75e0
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-04-02 22:25:54 -0300

    add initial migration

--- MessageBroker/Program.cs
+++ MessageBroker/Program.cs
@@ -1,4 +1,9 @@
+using MessageBroker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=MessageBroker.db"));
 
 var app = builder.Build();
 
